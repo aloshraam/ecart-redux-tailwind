dc5166186fb7028553274c199b004ab5cf6bb52f
aloshraam <aloshraam@gmail.com>
2025-01-21 08:29:12 +0530

    add search bar feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-
+import { searchProduct } from '../redux/slices/productSlice'
 
 const Header = ({insideHome}) => {
 
+  const dispatch = useDispatch()
   const userWishlist = useSelector(state => state.wishlistReducer)
   const userCart = useSelector(state=>state.cartReducer)
   return (
@@ -13,7 +14,7 @@ const Header = ({insideHome}) => {
       <ul className='flex-1 text-right'>
         {
           insideHome&&
-          <li className='list-none inline-block px-5'><input className='p-1 rounded' type="text" placeholder='Search products here...'/></li>
+          <li className='list-none inline-block px-5'><input onChange={e=>dispatch(searchProduct(e.target.value.toLowerCase()))} className='p-1 rounded text-black' type="text" placeholder='Search products here...'/></li>
         }
         <li className='list-none inline-block px-5'> <Link to={'/wishlist'}><i className='fa-solid fa-heart text-red-600 p-1'></i>Wishlist<span className='bg-black text-white px-1 ml-1 rounded'>{userWishlist?.length}</span></Link></li>
         <li className='list-none inline-block px-5'> <Link to={'/cart'}><i className='fa-solid fa-cart-plus text-green-700 p-1'></i>Cart<span className='bg-black text-white px-1 ml-1 rounded'>{userCart?.length}</span></Link></li>
